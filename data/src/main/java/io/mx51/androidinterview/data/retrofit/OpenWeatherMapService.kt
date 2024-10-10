8624a7be520d5631395f5095185c20783f808a18
yash-brilliant <yash@brilliant.tech>
2024-10-10 13:40:10 -0700

    Base changes
    Update build.gradle, strings.xml and leave parametrized URL args in OpenWeatherMapService.kt

--- data/src/main/java/io/mx51/androidinterview/data/retrofit/OpenWeatherMapService.kt
+++ data/src/main/java/io/mx51/androidinterview/data/retrofit/OpenWeatherMapService.kt
@@ -1,8 +1,12 @@
 package io.mx51.androidinterview.data.retrofit
 
 import retrofit2.http.GET
-
+import retrofit2.http.Query
 interface OpenWeatherMapService {
-    @GET("data/2.5/weather?q=sydney,AU&appid=2326504fb9b100bee21400190e4dbe6d&units=metric")
-    suspend fun getCurrentWeather(): OpenWeatherMapDTO
+    @GET("data/2.5/weather")
+    suspend fun getCurrentWeather(
+        @Query("units") units: String = "metric",
+        @Query("appid") appId: String = "2326504fb9b100bee21400190e4dbe6d",
+        @Query("q") city: String = "sydney,AU"
+    ): OpenWeatherMapDTO
 }
